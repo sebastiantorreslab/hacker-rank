f5c47c378824bc8241d94ab9c8f9c2026adf3d3f
sebastiantorreslab <str9371@gmail.com>
2024-02-12 23:59:12 -0500

    java regex 2 duplicated word

---
+++ src/main/java/com/practice/hackerrank/java_regex2_duplicate_words/Solution.java
@@ -0,0 +1,21 @@
+package com.practice.hackerrank.java_regex2_duplicate_words;
+
+public class Solution {
+
+    /*
+     In this challenge, we use regular expressions (RegEx) to remove instances of words that are repeated more than once, but retain the first occurrence of any case-insensitive repeated word. For example, the words love and to are repeated in the sentence I love Love to To tO code. Can you complete the code in the editor so it will turn I love Love to To tO code into I love to code?
+     * */
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
